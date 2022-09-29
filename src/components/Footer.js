e1413946087bc0dce555873b7339b61a37e8cd52
kenertj <tsoojkk@gmail.com>
2022-09-29 19:12:19 +0300

    Changes in footer and Getstarted section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Grid from '@mui/material/Unstable_Grid2';
-import { Box, Typography, Card, CardContent, TextField, Button } from '@mui/material';
+import { Box, Typography, Card, CardContent, TextField, Button, Link } from '@mui/material';
 import Logo from '../assets/logo.svg';
 import IconLocation from '../assets/icon-location.svg';
 import IconPhone from '../assets/icon-phone.svg';
@@ -8,37 +8,138 @@ import IconEmail from '../assets/icon-email.svg';
 
 const Footer = () => {
     return (
+
         <Box>
+
             <Box
                 component="img"
                 sx={{
                     height: 34,
+                    mt: "20vh",
+                    ml: "10vh"
                 }}
                 alt="Logo"
                 src={Logo}
             />
-            <Grid container spacing={3}>
+            <Grid container spacing={1} sx={{
+                mt: '5vh'
+            }}>
                 <Grid item xs>
                     <Box
-                        display="flex"
+                        display="inline-block"
                         justifyContent="center"
                         alignItems="center"
                         position="relative"
-                        minHeight="10vh"
+                        minHeight="1vh"
                         component="img"
                         margin="auto"
                         alt="Logo"
+
                         src={IconLocation}
 
                     />
+                    <Typography fontSize="13px" fontWeight="200" color="white" mt="5px" textAlign="center" display="inline">Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</Typography>
                 </Grid>
                 <Grid item xs>
+                    <Box
+                        display="inline-block"
+                        justifyContent="center"
+                        alignItems="center"
+                        position="relative"
+                        minHeight="1vh"
+                        component="img"
+                        margin="auto"
+                        alt="Logo"
+                        src={IconPhone}
 
-                </Grid>
-                <Grid item xs>
+                    />
+                    <Typography fontSize="13px" fontWeight="200" color="white" mt="5px" textAlign="center" display="inline-block" ml="2vh">+1-543-123-4567</Typography>
+                    <br />
+                    <Box
+                        display="inline-block"
+                        justifyContent="center"
+                        alignItems="center"
+                        position="relative"
+                        minHeight="1vh"
+                        component="img"
+                        margin="auto"
+                        alt="Logo"
+                        src={IconEmail}
 
+                    />
+                    <Typography fontSize="13px" fontWeight="200" color="white" mt="5px" textAlign="center" display="inline-block" ml="2vh">example@fylo.com</Typography>
                 </Grid>
                 <Grid item xs>
+                    <Link href="#" sx={{
+                        textDecoration: 'none',
+                        color: '#f9ffff',
+                        fontWeight: '400',
+                        ':hover': {
+                            fontWeight: '700',
+
+                        },
+                    }}>About Us</Link>
+                    <br />
+                    <Link href="#" sx={{
+                        textDecoration: 'none',
+                        color: '#f9ffff',
+                        fontWeight: '400',
+                        ':hover': {
+                            fontWeight: '700',
+
+                        },
+                    }}>Jobs</Link>
+                    <br />
+                    <Link href="#" sx={{
+                        textDecoration: 'none',
+                        color: '#f9ffff',
+                        fontWeight: '400',
+                        ':hover': {
+                            fontWeight: '700',
+
+                        },
+                    }}>Press</Link>
+                    <br />
+                    <Link href="#" sx={{
+                        textDecoration: 'none',
+                        color: '#f9ffff',
+                        fontWeight: '400',
+                        ':hover': {
+                            fontWeight: '700',
+
+                        },
+                    }}>Blog</Link>
+                </Grid>
+                <Grid item xs>
+                    <Link href="#" sx={{
+                        textDecoration: 'none',
+                        color: '#f9ffff',
+                        fontWeight: '400',
+                        ':hover': {
+                            fontWeight: '700',
+
+                        },
+                    }}>Contact Us</Link>
+                    <br />
+                    <Link href="#" sx={{
+                        textDecoration: 'none',
+                        color: '#f9ffff',
+                        fontWeight: '400',
+                        ':hover': {
+                            fontWeight: '700',
+
+                        },
+                    }}>Terms</Link>
+                    <br />
+                    <Link href="#" sx={{
+                        textDecoration: 'none',
+                        color: '#f9ffff',
+                        fontWeight: '400',
+                        ':hover': {
+                            fontWeight: '700',
+
+                        },
+                    }}>Privacy</Link>
 
 
                 </Grid>
--- src/components/Getstarted.js
+++ src/components/Getstarted.js
@@ -7,25 +7,34 @@ const Getstarted = () => {
     return (
         <Box sx={{
             margin: '0 auto',
-            maxWidth: '700px'
+            maxWidth: '700px',
+
+
+
         }}>
-            <Grid>
+            <Grid sx={{
+                position: 'absolute',
+                top: '330vh'
+            }}>
                 <Grid xs={12}>
-                    <Card sx={{ maxWidth: '100vh', backgroundColor: '#21293c' }}>
+                    <Card sx={{ maxWidth: '100vh', backgroundColor: '#1c2230' }}>
                         <CardContent>
-                            <Typography fontSize="30px" fontWeight="700" color="white" textAlign="center">Get early access today</Typography>
-                            <Typography fontSize="13px" fontWeight="200" color="white" mt="5px" textAlign="center">It only takes a minute to sign up and our free starter tier is extremely generous. If you have any <br /> questions, our support team would be happy to help you.</Typography>
-                            <TextField id="filled-basic" label="Email" variant="filled" sx={{
+                            <Typography fontSize="30px" fontWeight="700" color="white" mt="2vh" textAlign="center">Get early access today</Typography>
+                            <Typography fontSize="13px" fontWeight="200" color="white" mt="2vh" mb="4vh" textAlign="center">It only takes a minute to sign up and our free starter tier is extremely generous. If you have any <br /> questions, our support team would be happy to help you.</Typography>
+                            <TextField id="filled-basic" type="email" label="Email" variant="filled" sx={{
                                 backgroundColor: 'white',
                                 borderRadius: '23px',
-                                minWidth: '45vh',
-                                maxHeight: '7vh'
+                                minWidth: '55vh',
+                                maxHeight: '6vh',
+                                mb: '2vh'
                             }} />
                             <Button sx={{
                                 position: "relative",
                                 color: 'white',
                                 borderRadius: '23px',
-                                padding: '10px 80px',
+                                padding: '10px 30px',
+                                ml: '4vh',
+                                mr: '5vh',
                                 textTransform: 'none',
                                 backgroundImage: 'linear-gradient(#65e2d9,#339ecc)',
                                 ':hover': {
